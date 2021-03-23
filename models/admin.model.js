c2f69e001b5a7ec081e204f8c9d2546c21b009cb
ErwanFeltesse <er.feltesse@gmail.com>
2021-03-23 16:43:04 +0100

    admin model done

---
+++ models/admin.model.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose");
+const { isEmail } = require("validator");
+
+const adminSchema = new mongoose.Schema({
+  pseudo: {
+    type: String,
+    required: true,
+    minLength: 3,
+    maxLength: 55,
+    unique: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    validate: [isEmail],
+    lowercase: true,
+    trim: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    max: 1024,
+    minLength: 6,
+  },
+  bio: {
+    type: String,
+    max: 1024,
+  },
+});
+
+const AdminModel = mongoose.model('admin', adminSchema)
+module.exports = AdminModel
